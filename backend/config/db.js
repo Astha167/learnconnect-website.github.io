5e3c4d5f78cb8441999c1a13b76abb21e32b2de7
Astha Kumari <asthakumari605@gmail.com>
2025-11-12 16:59:23 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,11 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     console.log(`✅ Using database: ${conn.connection.name}`);
   } catch (error) {
-    console.error(`❌ MongoDB connection error: ${error.message}`);
+    console.error(`❌ MongoDB Connection Error: ${error.message}`);
     process.exit(1);
   }
 };
